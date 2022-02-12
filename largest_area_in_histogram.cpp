490bf0c2df060fb95e3966d8389d42730a3e8d28
Mohit Dharmik <dharmikmohit@gmail.com>
2022-02-12 15:23:34 +0530

    added alternate approach for largest area

--- largest_area_in_histogram.cpp
+++ largest_area_in_histogram.cpp
@@ -1,55 +1,107 @@
-// Maximum Rectangular Area in a Histogram
-// https://practice.geeksforgeeks.org/problems/maximum-rectangular-area-in-a-histogram-1587115620/1#
+// Max rectangle
+// https://practice.geeksforgeeks.org/problems/max-rectangle/1
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
-    void fill_next_smaller_element(long long* arr, int n, int* r)
+#define MAX 1000
+
+class Solution{
+    void fill_left(int* arr, int n, int* l)
     {
-        stack<long long> s;
-        s.push(n);
-        
-        for(int i = n-1; i >= 0; i--)
-        {
-            while(s.top() < n && arr[s.top()] >= arr[i])
-                s.pop();
-                    
-            r[i] = s.top();
-            s.push(i);
-        }
-    }
-    void fill_prev_smaller_element(long long* arr, int n, int* l)
-    {
-        stack<long long> s;
+        stack<int> s;
         s.push(-1);
         
         for(int i = 0; i < n; i++)
         {
             while(s.top() >= 0 && arr[s.top()] >= arr[i])
                 s.pop();
-                    
+            
             l[i] = s.top();
             s.push(i);
         }
     }
-    public:
-    //Function to find largest rectangular area possible in a given histogram.
-    long long getMaxArea(long long arr[], int n)
+    void fill_right(int* arr, int n, int* r)
     {
-        // Your code here
-        long long area = 0;
-        int* r = new int[n];
-        int* l = new int[n];
+        stack<int> s;
+        s.push(n);
         
-        fill_next_smaller_element(arr, n, r);
-        fill_prev_smaller_element(arr, n, l);
+        for(int i = n-1; i >= 0; i--)
+        {
+            while(s.top() < n && arr[s.top()] >= arr[i])
+                s.pop();
+                
+            r[i] = s.top();
+            s.push(i);
+        }
+    }
+  public:
+    int maxArea(int M[MAX][MAX], int n, int m) {
+        int hist[m] = {0};
+        int l[m], r[m];
+        int max_area = 0;
         
         for(int i = 0; i < n; i++)
-            area = max(area, arr[i] * (r[i] - l[i] - 1));
+        {
+            //fill hist
+            for(int j = 0; j < m; j++)
+                hist[j] = (M[i][j])? (hist[j]+1) : 0;
+            
+            //fill l and r
+            fill_left(hist, m, l);
+            fill_right(hist, m, r);
+            
+            
+            //find areas for each elements
+            for(int j = 0; j < m; j++)
+                max_area = max(max_area, hist[j] * (r[j] - l[j] - 1));
+        }
         
-        delete r, l;
-        return area;
+        return max_area;
     }
 };
+
+//fills l and r array in one pass
+class Alernate_Solution
+{
+    void fill_lr(int* arr, int n, vector<int>& l, vector<int>& r)
+    {
+        stack<int> s;
+        s.push(-1);
+        
+        for(int i = 0; i < n; i++)
+        {
+            while(s.top() >= 0 && arr[s.top()] > arr[i])
+            {
+                r[s.top()] = i;
+                s.pop();
+            }
+            
+            l[i] = s.top();
+            
+            s.push(i);
+        }
+    }
+  public:
+    int maxArea(int M[MAX][MAX], int n, int m) {
+        int hist[m] = {0};
+        int max_area = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            vector<int> l(m, -1), r(m, m);
+            //fill hist
+            for(int j = 0; j < m; j++)
+                hist[j] = (M[i][j])? (hist[j]+1) : 0;
+            
+            //fill l and r
+            fill_lr(hist, m, l, r);
+            
+            //find areas for each elements
+            for(int j = 0; j < m; j++)
+                max_area = max(max_area, hist[j] * (r[j] - l[j] - 1));
+        }
+        
+        return max_area;
+    }
+}
